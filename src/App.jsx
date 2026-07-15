0d17aa33d5f4bdb9578ae7f671754eaa800002df
AttiaPrograms <1234attia.khan@gmail.com>
2026-07-15 16:21:35 +0500

    Add comments and complete some parts of dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,8 @@
-import React from "react";
 import Dashboard from "./AdminPanel/Dashboard";
-<<<<<<< HEAD
-import DoctorManagement from "./AdminPanel/DoctorManagement";
-import PatientManagement from "./AdminPanel/PatientManagement"; 
-=======
-import Navbar from "./HomePage/Navbar";
->>>>>>> 4bf2c0f053735438c3ddeb2e92100ef5358fc391
 import "./index.css";
 
 function App() {
-  return(
-  <>
-  <Dashboard />;
-  <Navbar/>
-  </>
-  )
+  return <Dashboard />;
 }
 
 export default App;
